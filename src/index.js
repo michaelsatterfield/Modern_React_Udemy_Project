e5ef8512c4132fe58e6e87a5bea458fd97d9e256
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-12-03 20:53:40 -0600

    added react componet to index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import ReactDOM from 'react-dom';
 import React from 'react'
 
 // create react components//
+const App = () => {
+    return <div>Hello World!</div>;
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
